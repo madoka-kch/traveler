99d7a5e5a0c22ae349329b9242c1d478fedbeec2
madoka-kch <182390705+madoka-kch@users.noreply.github.com>
2024-11-01 06:01:55 +0900

    マイページの実装

---
+++ db/migrate/20241031201638_add_user_idto_posts.rb
@@ -0,0 +1,4 @@
+class AddUserIdtoPosts < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_31_073524) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_31_201638) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
