cdb4fac412619c6a835d3b9e19645c6c6300d037
mdkkch <182390705+madoka-kch@users.noreply.github.com>
2024-12-08 14:55:49 +0900

    DM機能のレイアウト作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = false
+
+  config.assets.debug = true
+  config.assets.compile = true
+
+
+
+
 end
